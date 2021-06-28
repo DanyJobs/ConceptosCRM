286628be13eeb9af00c673b08eda93e566ad3acb
DanyJobs <juan100mata@gmail.com>
2021-06-28 15:53:39 -0500

    Inicio

---
+++ WebFacturaMvc/Js/Categoria.js
@@ -0,0 +1,4 @@
+﻿/// <reference path="E:\Proycto\FACTURA_JHONNYv2\WebFacturaMvc\Views\Producto\Create.cshtml" />
+$("#btnNuevo").click(function (eve) {
+    $("#modal-content").load("/Producto/Create");
+});
